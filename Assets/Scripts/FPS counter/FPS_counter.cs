e83ce55656c7e17b25e059ad208e2dae587140c0
Jörn Alexander Quent <alexander.quent@rub.de>
2022-09-27 07:39:43 +0800

    Update FPS_counter.cs

--- Assets/Scripts/FPS counter/FPS_counter.cs
+++ Assets/Scripts/FPS counter/FPS_counter.cs
@@ -10,28 +10,44 @@
         public int samples;
         public Text displayTextCurrent;
         public Text displayTextAverage;
-        public int index = 0;
+        public int index1 = 0;
+        public float refreshRate = 0.2f;
+        private float timer;
         public int temporaryStorageCapacity = 500; 
         private List<int> temporaryFrameRateStorage = new List<int>();
         private List<double> permanentStorage = new List<double>();
+
+        void Start(){
+            // Set timer to refresh rate
+            timer = refreshRate;
+        }
      
         public void Update (){
+            // Calculate current FPS
             float current = 0;
             current = (int)(1f / Time.unscaledDeltaTime);
             currentFrameRate = (int)current;
-            displayTextCurrent.text = currentFrameRate.ToString() + " FPS";
 
-            // Commit to temporary storage
-            if(index <= temporaryStorageCapacity){
+            // Update UI text 
+            timer = timer - Time.deltaTime; // Coubt time for refresh rate
+            if(timer <= 0){
+                displayTextCurrent.text = currentFrameRate.ToString() + " FPS";
+                timer = refreshRate;
+            }
+
+            // Commit current FPS to temporary storage
+            if(index1 <= temporaryStorageCapacity){
                 temporaryFrameRateStorage.Add(currentFrameRate);
-                index = index + 1;
+                index1 = index1 + 1;
             } else {
+                // If above index calculate average of the full temporary storage and save in long term storage. 
                 permanentStorage.Add(temporaryFrameRateStorage.Average());
                 avgFrameRate =  Mathf.Round((float)permanentStorage.Average());
                 samples = permanentStorage.Count; // Calculate the number of samples
-                displayTextAverage.text =avgFrameRate.ToString() + " FPS (AVG based on " +  samples+ ")";
-                // Reset
-                index = 0;
+                displayTextAverage.text =avgFrameRate.ToString() + " FPS (AVG based on " + samples + ")";
+                
+                // Reset the values
+                index1 = 0;
                 temporaryFrameRateStorage = new List<int>();
             }
         }
